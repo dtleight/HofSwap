c35a6589d6277337a5240ee6ce9205db34a5341c
dtleight <dtleight@gmail.com>
2020-10-15 16:02:49 -0400

    Updated AcccountPage.dart
    Co-Authored-By: Angelina Boudouvas <71291450+aboudouvas1@users.noreply.github.com>
    Co-Authored-By: Ashley Smith <70593237+asmith422@users.noreply.github.com>
    Co-Authored-By: Aaron George <66791738+AaronCoder1213@users.noreply.github.com>
    Co-Authored-By: XJiang0809 <71741670+XJiang0809@users.noreply.github.com>

--- lib/Pages/AccountPage.dart
+++ lib/Pages/AccountPage.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hofswap/Objects/Account.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatelessWidget
+{
+  Account account = new Account.instantiate("Dalton Leight", null,null,null, 5, "dleight1@pride.hofstra.edu");
   @override
   Widget build(BuildContext context)
   {
+    //bottom: TabBar(tabs:[Tab(text: account.name,)],),
     return Scaffold
       (
-      body: Text("Account Page"),
+      appBar: AppBar(title: Text("My Account"),),
+      body: Column(children:
+      [
+        Center(
+          child: CircleAvatar(
+            backgroundImage: NetworkImage("https://www.hofstra.edu/images/academics/colleges/seas/computer-science/csc-sjeffr2.jpg"),
+            radius: 100,
+          ),
+        ),
+      ],),
     );
   }
 }
