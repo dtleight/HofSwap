38885a21db293d84836148d25cfa91a01afa68a7
Aaron George <66791738+AaronCoder1213@users.noreply.github.com>
2020-10-15 13:57:57 -0400

    Started Account Page and fixed small error when displaying authors

---
+++ lib/Pages/AccountPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hofswap/Pages/FocusedStoreView.dart';
+import 'package:hofswap/Singeltons/DatabaseRouting.dart';
+import 'package:hofswap/Utilities/TextbookAPILoader.dart';
+import '../Objects/Textbook.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AccountPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage>
+{
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+
+}
