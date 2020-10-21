5c51ad5c83012360e8a4d584a6b8358bd231b18d
Ashley Smith <70593237+asmith422@users.noreply.github.com>
2020-10-20 23:22:43 -0400

    database progress

--- lib/Singeltons/DatabaseRouting.dart
+++ lib/Singeltons/DatabaseRouting.dart
@@ -5,6 +5,7 @@ import '../Objects/Textbook.dart';
 
 class DatabaseRouting
 {
+
   static final DatabaseRouting _db = DatabaseRouting._internal();
  List<Textbook> textbooks;
   Account account = new Account.instantiate("Scott Jefferys", null,null,null, 5, "scott.m.jefferys@hofstra.edu",);
@@ -25,8 +26,43 @@ class DatabaseRouting
     return await ref.getDocuments();
   }
 
+  class AddUser (Account account){
+    print("Database write started");
 
-  void createUser(Account account)
+    final String email;
+    final String id;
+    final String name;
+    final String password;
+
+    AddUser(this.email, this.id, this.name, this.password);
+    /*
+    @override
+    Widget build(BuildContext context) {
+    // Create a CollectionReference called users that references the firestore collection
+        CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+        Future<void> addUser() {
+          // Call the user's CollectionReference to add a new user
+          return users
+              .add({
+          'full_name': fullName, // John Doe
+          'company': company, // Stokes and Sons
+          'age': age // 42
+          })
+              .then((value) => print("User Added"))
+              .catchError((error) => print("Failed to add user: $error"));
+        }
+
+        return FlatButton(
+          onPressed: addUser,
+          child: Text(
+          "Add User",
+          ),
+        );
+    }
+     */
+  }
+  avoid createUser(Account account)
   {
     print("Database write started");
     Firestore.instance.collection("users").document(account.name).setData(
