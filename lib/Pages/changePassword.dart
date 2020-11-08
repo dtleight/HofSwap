c2aedb2ebba79bd6aba9cb597a25ff216310089a
XJiang0809 <71741670+XJiang0809@users.noreply.github.com>
2020-11-08 11:32:16 -0500

    minor update on change password

---
+++ lib/Pages/changePassword.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChangePassword extends StatelessWidget
+{
+  ChangePassword();
+  @override
+  Widget build(BuildContext context) {
+    // ignore: non_constant_identifier_names
+    return Scaffold(
+      appBar: AppBar(title: Text('Change Your Password'),
+        backgroundColor: Colors.blue,),
+
+
+    );
+  }
+}
